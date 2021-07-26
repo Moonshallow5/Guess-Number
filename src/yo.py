7154c1409b4db297b21f662a30192be467f9efa4
Sandeep Singh <ssandeepssingh55@gmail.com>
2021-07-26 21:47:32 +0800

    ddd

--- src/yo.py
+++ src/yo.py
@@ -1,8 +1,12 @@
 print("yo")
+
+
 def write():
     print("--------------------------------------------------------------------------")
     print("Do you want to choose how many tries you get? ")
     print("Enter Y to choose the number of tries and N to guess with infinite tries")
     print("Please enter -1 if you want to concede")
     print("--------------------------------------------------------------------------")
+
+
 write()
