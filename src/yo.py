065c06641a1aa5c9d18a582a1704ef146cf11e8a
Sandeep Singh <ssandeepssingh55@gmail.com>
2021-07-28 13:31:49 +0800

    added output for higher lower numbers

--- src/yo.py
+++ src/yo.py
@@ -1,3 +1,5 @@
+import random
+
 
 def write():
     print("--------------------------------------------------------------------------")
@@ -6,4 +8,44 @@ def write():
     print("Please enter -1 if you want to concede")
     print("--------------------------------------------------------------------------")
 
+
 write()
+
+
+def number():
+    computer_number = random.randrange(0, 100)
+    return computer_number
+
+
+computer = number()
+
+
+def input_more(player_input):
+    if player_input < computer:
+        print("Go higher")
+
+
+def pick_less(player_input):
+    if player_input > computer:
+        print("Go lower")
+
+
+def pick_equal(player_input):
+    if player_input == computer:
+        return True
+
+
+def pick_loop():
+    i = 0
+    print(computer)
+    while i == 0:
+        player_input = int(input("Enter: "))
+        print(player_input)
+        input_more(player_input)
+        pick_less(player_input)
+        if pick_equal(player_input):
+            print("Well done")
+            break
+
+
+pick_loop()
