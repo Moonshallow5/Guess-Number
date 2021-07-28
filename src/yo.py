929992b883296a7caf8af8913939f97b13d7709a
Sandeep Singh <ssandeepssingh55@gmail.com>
2021-07-28 14:16:47 +0800

    add

--- src/yo.py
+++ src/yo.py
@@ -23,11 +23,13 @@ computer = number()
 def input_more(player_input):
     if player_input < computer:
         print("Go higher")
+        return True
 
 
 def pick_less(player_input):
     if player_input > computer:
         print("Go lower")
+        return True
 
 
 def pick_equal(player_input):
@@ -35,16 +37,20 @@ def pick_equal(player_input):
         return True
 
 
-def pick_loop():
+def pick_loop(tries=0):
     i = 0
     print(computer)
     while i == 0:
         player_input = int(input("Enter: "))
         print(player_input)
-        input_more(player_input)
-        pick_less(player_input)
+        if input_more(player_input):
+            tries += 1
+        if pick_less(player_input):
+            tries += 1
         if pick_equal(player_input):
             print("Well done")
+            tries += 1
+            print("Ties: "+str(tries))
             break
 
 
