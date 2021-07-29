b417e40cd71b3cd7ec38f9b66eea576f08b92129
Sandeep Singh <ssandeepssingh55@gmail.com>
2021-07-29 17:19:41 +0800

    wd

--- src/yo.py
+++ src/yo.py
@@ -13,6 +13,7 @@ def number():
     global computer
     computer = random.randrange(0, 100)
 
+number_games=0
 
 def input_more(player_input):
     if player_input < computer:
@@ -40,15 +41,19 @@ tries = 0
 
 def pick_tries():
     number()
+    global number_games
     global i
     i = 0
     global tries
+    tries=0
     global score_computer
     global score_human
 
     while i < z:
         print(computer)
         player_input = int(input("Enter: "))
+        if player_input==-1:
+            break
         if input_more(player_input):
             tries += 1
             i += 1
@@ -66,6 +71,8 @@ def pick_tries():
         print("You have exceeded the number of guesses which is " + str(z))
         score_computer += 1
         print("Score of computer is " + str(score_computer) + " and score of human is " + str(score_human))
+    number_games+=1
+    print("Number of games played: "+str(number_games))
     play = input("Would u play again? Type Y to play again ")
     if play == "Y":
         start()
@@ -76,6 +83,7 @@ def pick_tries():
 def pick_loop():
     number()
     i = 0
+    global number_games
     global tries
     tries = 0
     while i == 0:
@@ -92,6 +100,8 @@ def pick_loop():
             tries += 1
             print("Your number of tries is : " + str(tries))
             break
+    number_games+=1
+    print("Number of games played: "+str(number_games))
     play = input("Would u play again? Type Y to play again ")
     if play == "Y":
         start()
